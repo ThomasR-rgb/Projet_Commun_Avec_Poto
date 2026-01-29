cc6ad115ad188055da721b787547f3b9509f1b56
ThomasR <2543130@cstj.qc.ca>
2026-01-29 14:16:44 -0500

    sa

---
+++ RPGPD_La_Solution/RPGPD_Le_Jeu/Mobs.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGPD_Le_Jeu
+{
+    internal class Mobs
+    {
+        public int HP;
+        public int Attack;
+        public int exp;
+        public string Nom;
+        public int difficulty;
+
+        public Mobs (int HP, int Attack, int exp, int difficulty, string Nom)
+        {
+            this.HP = HP;
+            this.Attack = Attack;
+            this.exp = exp;
+            this.difficulty = difficulty;
+            this.Nom = Nom;
+        }
+
+        
+
+
+
+
+
+    }
+}
