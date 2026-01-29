2f68754fb5a836263496996dffad7eb2510d47fe
ThomasR <2543130@cstj.qc.ca>
2026-01-28 22:20:25 -0500

    dasd

--- RPGPD_La_Solution/RPGPD_Le_Jeu/Program.cs
+++ RPGPD_La_Solution/RPGPD_Le_Jeu/Program.cs
@@ -8,8 +8,34 @@
         static void Main(string[] args)
         {
             // Variables
-            
 
+
+            // Début du programme
+            do
+            {
+                Intro();
+
+
+
+
+            } while (true);
         }
+
+        // Fonctions
+
+        // Début fonction Introduction
+        private static void Intro()
+        {
+            Console.WriteLine("Hello you... (Introduction)");
+        }
+        // Fin fonction Introduction
+
+        // Début fonction Menu
+        private static int Menu(int choix)
+        {
+
+            return choix;
+        }
+        // Fin fonction Menu
     }
 }
