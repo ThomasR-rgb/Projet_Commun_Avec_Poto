a301e052b3ea930009d98bc0b8378a4080537950
ThomasR <2543130@cstj.qc.ca>
2026-01-29 14:27:51 -0500

    as

--- RPGPD_La_Solution/RPGPD_Le_Jeu/Mobs.cs
+++ RPGPD_La_Solution/RPGPD_Le_Jeu/Mobs.cs
@@ -8,22 +8,43 @@ namespace RPGPD_Le_Jeu
 {
     internal class Mobs
     {
-        public int HP;
-        public int Attack;
-        public int exp;
-        public string Nom;
         public int difficulty;
 
-        public Mobs (int HP, int Attack, int exp, int difficulty, string Nom)
+        public Mobs (int difficulty)
         {
-            this.HP = HP;
-            this.Attack = Attack;
-            this.exp = exp;
             this.difficulty = difficulty;
-            this.Nom = Nom;
         }
 
-        
+        public int Générer_choix_Mob()
+        {
+            Random random = new Random();
+            int choix = random.Next(1, 4);
+            return choix;
+        }
+        public int Générer_HP(int difficulty, int choix)
+        {
+            
+            switch (difficulty)
+            {
+                case 1:
+                    switch (choix)
+                    {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                    }
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+
+            }
+        }
+
 
 
 
