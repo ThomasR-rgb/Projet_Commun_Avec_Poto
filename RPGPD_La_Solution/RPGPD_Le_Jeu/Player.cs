3694ee66528bb28927d8f0e59cf487451e647931
2543130 <2543130@cstj.qc.ca>
2026-02-02 11:12:03 -0500

    Fini liste des spells fonction pour player maxhp fonction pour player maxMP

--- RPGPD_La_Solution/RPGPD_Le_Jeu/Player.cs
+++ RPGPD_La_Solution/RPGPD_Le_Jeu/Player.cs
@@ -117,6 +117,127 @@ namespace RPGPD_Le_Jeu
             } // Fin switch
             return bloquehealPlayer;
         } // Fin Generer_Defense_Player
+        public int MaxHPPlayerScale(int difficulty, int playerClass)
+        {
+            int MaxHP = 0;
+            switch(playerClass)
+            {
+                case 1: // Fighter
+                    switch(difficulty) // level
+                    {
+                        case 1:
+                            MaxHP = 25;
+                            return MaxHP;
+                        case 2:
+                            MaxHP = 30;
+                            return MaxHP;
+                        case 3:
+                            MaxHP = 35;
+                            return MaxHP;
+                    }
+                    break;
+                case 2: // White Mage
+                    switch (difficulty) // level
+                    {
+                        case 1:
+                            MaxHP = 22;
+                            return MaxHP;
+                        case 2:
+                            MaxHP = 27;
+                            return MaxHP;
+                        case 3:
+                            MaxHP = 31;
+                            return MaxHP;
+                    }
+                    break;
+                case 3: // Dark Mage
+                    switch (difficulty) // level
+                    {
+                        case 1:
+                            MaxHP = 20;
+                            return MaxHP;
+                        case 2:
+                            MaxHP = 24;
+                            return MaxHP;
+                        case 3:
+                            MaxHP = 28;
+                            return MaxHP;
+                    }
+                    break;
+            }
+            return MaxHP;
+        } // fin fonction Player HP Scale
 
-    }
+        public int BaseMPPlayer(int difficulty, int playerclass)
+        {
+            int BaseMP = 0;
+            switch(playerclass)
+            {
+                case 1: // Fighter
+                    switch(difficulty)
+                    {
+                        case 1: // lvl1
+                            BaseMP = 1;
+                            return BaseMP;
+                        case 2:
+                            BaseMP = 1;
+                            return BaseMP;
+                        case 3:
+                            BaseMP = 2;
+                            return BaseMP;
+                    }
+                    break;
+                case 2: // White Mage
+                    switch (difficulty)
+                    {
+                        case 1: // lvl1
+                            BaseMP = 2;
+                            return BaseMP;
+                        case 2:
+                            BaseMP = 3;
+                            return BaseMP;
+                        case 3:
+                            BaseMP = 5;
+                            return BaseMP;
+                    }
+                    break;
+                case 3: // Dark Mage
+                    switch (difficulty)
+                    {
+                        case 1: // lvl1
+                            BaseMP = 3;
+                            return BaseMP;
+                        case 2:
+                            BaseMP = 5;
+                            return BaseMP;
+                        case 3:
+                            BaseMP = 7;
+                            return BaseMP;
+                    }
+                    break;
+            }
+            return BaseMP;
+        }
+
+        /* liste des spells possible
+         * fighter: lvl1 magic sword +1DP sur attack jusqua fin combat
+         *          lvl2 cure +10-15 HP
+         * White mage: lvl1 1MP Fire Bolt 5-10DP   1MP Heal +8-13 HP
+         *             lvl2 2MP Divine Grace si meurt le tour où le spell est cast, reviens a la vie avec 50% HP
+         *             lvl3 3MP Smite 10-50DP
+         * Dark mage: lvl1 1MP Vampire Touch 2-5DP + Heal 
+         *            lvl2 2MP Reflect -[2-3] DP physique reçue jusqu'à la fin du combat  2MP Ice Beam 9-14DP
+         *            lvl3 7MP Kill Tue le truc en face
+         */
+        public int spellJoueur(int playerclass)
+        {
+            int intdivinduspell = 0;
+            switch(playerclass)
+            {
+                
+            }
+            return intdivinduspell;
+        }
+
+    } // pas sortir de ça
 }
